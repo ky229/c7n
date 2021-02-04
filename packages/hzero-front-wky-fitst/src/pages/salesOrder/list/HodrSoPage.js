68169bd724a37e355b4788af7cb6d2dd76807f1f
吴开云 <kaiyun.wu@hand-china.com>
2021-02-04 20:12:46 +0800

    [add]修改DS编写路径

--- packages/hzero-front-wky-fitst/src/pages/salesOrder/list/HodrSoPage.js
+++ packages/hzero-front-wky-fitst/src/pages/salesOrder/list/HodrSoPage.js
@@ -13,9 +13,234 @@ export default class FirstPage extends Component {
     this.state = {};
   }
 
-  hodrSoLineDS = new DataSet({ ...HodrSoLineDS() });
 
-  hodrSoHeaderDS = new DataSet({ ...HodrSoHeaderDS() });
+  hodrSoLineDS = new DataSet({
+    autoQuery: false,
+    autoCreate: true,
+    primaryKey: 'soLineId',
+    fields: [
+      {
+        name: 'soLineId',
+        type: 'number',
+        label: '销售订单行ID',
+      },
+      {
+        name: 'soHeaderId',
+        type: 'number',
+        label: '销售订单头ID',
+      },
+      {
+        name: 'lineNumber',
+        label: '行号',
+        type: 'number',
+      },
+      {
+        name: 'itemId',
+        label: '产品ID',
+        type: 'number',
+      },
+      {
+        name: 'itemName',
+        label: '产品名称',
+        type: 'string',
+      },
+      {
+        name: 'orderQuantity',
+        label: '数量',
+        type: 'number',
+      },
+      {
+        name: 'orderQuantityUom',
+        label: '产品单位',
+        type: 'string',
+      },
+      {
+        name: 'unitSellingPrice',
+        label: '销售单价',
+        type: 'number',
+      },
+      {
+        name: 'description',
+        label: '备注',
+        type: 'string',
+      },
+      {
+        name: 'addition1',
+        label: '附件信息1',
+        type: 'string',
+      },
+      {
+        name: 'addition2',
+        label: '附件信息2',
+        type: 'string',
+      },
+      {
+        name: 'addition3',
+        label: '附件信息3',
+        type: 'string',
+      },
+      {
+        name: 'addition4',
+        label: '附件信息4',
+        type: 'string',
+      },
+      {
+        name: 'addition5',
+        label: '附件信息5',
+        type: 'string',
+      },
+    ],
+    queryFields: [
+      {
+        name: 'lineNumber',
+        label: '行号',
+        type: 'number',
+      },
+      {
+        name: 'itemName',
+        label: '产品名称',
+        type: 'string',
+      },
+      {
+        name: 'orderQuantity',
+        label: '数量',
+        type: 'number',
+      },
+      {
+        name: 'description',
+        label: '备注',
+        type: 'string',
+      },
+    ],
+    transport: {
+      submit: {
+        url: '/soLine/queries',
+        method: 'post',
+      },
+      read: {
+        url: '/soLine/queries',
+        method: 'get',
+      },
+      create: {
+        url: '/soLine/mutations',
+        method: 'put',
+      },
+      update: ({ data }) =>
+        data.length
+          ? {
+            url: `/soLine/mutations/${data[0].userId}`,
+            data: data[0],
+          }
+          : null,
+      destroy: {
+        url: '/soLine/mutations',
+        method: 'delete',
+      },
+    },
+  });
+
+  hodrSoHeaderDS = new DataSet({
+    autoQuery: false,
+    primaryKey: 'soHeaderId',
+    fields: [
+      {
+        name: 'soHeaderId',
+        type: 'number',
+        label: '销售订单头ID',
+      },
+      {
+        name: 'orderNumber',
+        label: '订单编号',
+        type: 'string',
+      },
+      {
+        name: 'companyId',
+        label: '公司ID',
+        type: 'number',
+      },
+      {
+        name: 'companyName',
+        label: '公司名称',
+        type: 'string',
+      },
+      {
+        name: 'customerId',
+        label: '客户ID',
+        type: 'number',
+      },
+      {
+        name: 'customerName',
+        label: '客户名称',
+        type: 'string',
+      },
+      { name: 'orderDate', type: 'date', label: '订单日期' },
+      {
+        name: 'orderStatus',
+        label: '订单状态',
+        type: 'string',
+      },
+      {
+        name: 'orderPrice',
+        label: '订单金额',
+        type: 'number',
+      },
+    ],
+    queryFields: [
+      {
+        name: 'orderNumber',
+        label: '订单编号',
+        type: 'string',
+      },
+      {
+        name: 'orderStatus',
+        label: '订单状态',
+        type: 'string',
+      },
+      {
+        name: 'companyName',
+        label: '公司名称',
+        type: 'string',
+      },
+      {
+        name: 'customerName',
+        label: '客户名称',
+        type: 'string',
+      },
+    ],
+    transport: {
+      submit: {
+        url: '/soHeader/queries',
+        method: 'post',
+      },
+      read: {
+        url: '/soHeader/queries',
+        method: 'get',
+      },
+      create: {
+        url: '/soHeader/mutations',
+        method: 'put',
+      },
+      update: ({ data }) =>
+        data.length
+          ? {
+            url: `/dataset/user/mutations/${data[0].userId}`,
+            data: data[0],
+          }
+          : null,
+      destroy: {
+        url: '/dataset/user/mutations',
+        method: 'delete',
+      },
+      exports: {
+        url: 'http://gitee.com/xurime/excelize/raw/master/test/SharedStrings.xlsx',
+        method: 'get',
+      },
+    },
+    children: {
+      hodrSoLins: this.hodrSoLineDS,
+    },
+  });
+
 
   componentDidMount() {
     const lineData = [
@@ -32,7 +257,7 @@ export default class FirstPage extends Component {
         addition1: '2222-2',
       },
     ];
-    this.hodrSoLineDS.loadData(lineData, lineData.length);
+    this.hodrSoLineDS.loadData(lineData, 2);
     const headerData = [
       {
         soHeaderId: 1,
@@ -43,7 +268,7 @@ export default class FirstPage extends Component {
         orderNumber: '2222',
       },
     ];
-    this.hodrSoHeaderDS.loadData(headerData, headerData.length);
+    this.hodrSoHeaderDS.loadData(headerData, 2);
   }
 
   openModal = (record) => {
@@ -54,12 +279,12 @@ export default class FirstPage extends Component {
       children: (
         <Tabs>
           <TabPane tab="行基本信息">
-            <Table buttons={['add', 'delete']} dataSet={this.HodrSoLineDS} rowHeight={40}>
+            <Table buttons={['add', 'delete']} dataSet={this.hodrSoLineDS} rowHeight={40}>
               <Column name="lineNumber" editor />
             </Table>
           </TabPane>
           <TabPane tab="行拓展信息">
-            <Table dataSet={this.HodrSoLineDS} rowHeight={40}>
+            <Table dataSet={this.hodrSoLineDS} rowHeight={40}>
               <Column name="addition1" editor width={150} />
             </Table>
           </TabPane>
@@ -72,7 +297,21 @@ export default class FirstPage extends Component {
 
   buttons = ['add', 'delete'];
 
-  // eslint-disable-next-line no-unused-vars
+  renderEdit = () => {
+    return (
+      <Button
+        funcType="flat"
+        icon="mode_edit"
+        onClick={this.editUser}
+        size="small"
+      />
+    );
+  };
+
+
+  editUser = () => {
+    this.openModal();
+  };
   commands = ({ record }) => ['edit', ['delete', { color: 'red' }]];
 
   render() {
@@ -87,7 +326,7 @@ export default class FirstPage extends Component {
           >
             <Column name="soHeaderId" />
             <Column name="orderNumber" editor width={150} />
-            <Column header="编辑行信息" align="center" renderer={this.openModal} lock="right" />
+            <Column header="编辑行信息" align="center" renderer={this.renderEdit} lock="right" />
           </Table>
         </Content>
       </Fragment>
